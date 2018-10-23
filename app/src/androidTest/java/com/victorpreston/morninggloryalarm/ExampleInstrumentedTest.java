3ff54a77c93449eb8db286653e10c52cbe2b748c
Victor Preston <vbpreston@gmail.com>
2018-10-23 10:57:11 -0400

    Added gradle files for MorningGloryAlarm

--- app/src/androidTest/java/com/victorpreston/morninggloryalarm/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/victorpreston/morninggloryalarm/ExampleInstrumentedTest.java
@@ -3,6 +3,7 @@ package com.victorpreston.morninggloryalarm;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,11 +16,16 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+
+
 public class ExampleInstrumentedTest {
+    public static final String TAG = "ExampleInstrumentedText";
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+        Log.d(TAG,"appContext is: " + appContext.toString());
 
         assertEquals("com.victorpreston.morninggloryalarm", appContext.getPackageName());
     }
--- app/src/test/java/com/victorpreston/morninggloryalarm/ExampleUnitTest.java
+++ app/src/test/java/com/victorpreston/morninggloryalarm/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.victorpreston.morninggloryalarm;
 
+import android.util.Log;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +12,11 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    public static final String TAG = "ExampleUnitTest";
+
     @Test
     public void addition_isCorrect() throws Exception {
+        Log.d(TAG,"addition_is...");
         assertEquals(4, 2 + 2);
     }
 }
